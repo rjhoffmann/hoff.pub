bfc345bbd3c43199e79b3b6b4b2ff2c02a26523e
Ryan Hoffmann <ryan.hoffmann@airwis.com>
2018-02-23 12:54:52 -0600

    bugfix: webfontloader error during build

--- src/App.js
+++ src/App.js
@@ -2,42 +2,16 @@ import React from 'react';
 import { Router } from 'react-static';
 import { hot } from 'react-hot-loader';
 
-import WebfontLoader from '@dr-kobros/react-webfont-loader';
-
 import 'normalize.css';
 
 import Root from './containers/root';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      webfont: {
-        google: {
-          families: [
-            'Cardo:400,400i,700',
-            'Josefin Sans:100,100i,300,300i,400,400i,600,600i,700,700i'
-          ]
-        }
-      },
-      isLoading: true,
-    };
-
-    this.webfontCallback = this.webfontCallback.bind(this);
-  }
-
-  webfontCallback(status) {
-    this.setState({ isLoading: status === 'loading' })
-  }
-
   render() {
     return (
-      <WebfontLoader config={this.state.webfont} onStatus={this.webfontCallback}>
-        <Router>
-          <Root isLoading={this.state.isLoading} />
-        </Router>
-      </WebfontLoader>
+      <Router>
+        <Root />
+      </Router>
     )
   }
 }
--- src/containers/root.js
+++ src/containers/root.js
@@ -5,12 +5,6 @@ import Routes from 'react-static-routes';
 
 class Root extends React.Component {
   render() {
-    if(this.props.isLoading) {
-      return (
-        <div>Loading</div>
-      )
-    }
-
     return (
       <div>
         <nav>
